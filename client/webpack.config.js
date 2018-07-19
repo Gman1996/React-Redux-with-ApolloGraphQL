d7903951d661681e0055503069b5b1d2b1f3fb77
Gman1996 <dayoagbolade@gmail.com>
2018-07-19 16:32:14 +0100

    Initial Commit

---
+++ client/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: "./src/index.html"
+});
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundled.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  plugins: [htmlPlugin]
+};
